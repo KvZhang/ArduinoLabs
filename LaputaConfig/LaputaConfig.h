8d8ab6ba30bb971de82612366fc78ecd920f3c7b
KvZhang <KvZhang@users.noreply.github.com>
2017-07-16 10:47:55 +0800

    [Add]add two librarys
    1. Add ATparser:
     "AT", return "OK"
     "AT+Cxx", return "OK+Cxx", xx is number 0~16
     "AT+Nxxx", return "OK+Nxxx" ,xxx is number 0~255
     "AT+Rx",  return "OK+Cxx,Nxxx"
    2. Add LaputaConfig,
    Add a sructure in EEPROM to storage Channels and addrs
    +typedef struct
    +{
    +	uint8_t room_addr;
    +	uint8_t me_addr;
    +	uint8_t sys_rsv[12];
    +	uint16_t count;
    +	uint8_t user_data[14];
    +	uint16_t checksum;
    +}EEPROM_STRUCT;

---
+++ LaputaConfig/LaputaConfig.h
@@ -0,0 +1,38 @@
+/*
+ * LaputaConfig.h
+ *
+ *  Created on: 2017Äê5ÔÂ14ÈÕ
+ *      Author: zhangyu
+ */
+
+#ifndef LAPUTACONFIG_H_
+#define LAPUTACONFIG_H_
+
+#include "Arduino.h"
+#include "EEPROM.h"
+
+typedef struct
+{
+	uint8_t room_addr;
+	uint8_t me_addr;
+	uint8_t sys_rsv[12];
+	uint16_t count;
+	uint8_t user_data[14];
+	uint16_t checksum;
+}EEPROM_STRUCT;
+
+#define EEPROM_ADDR (0)
+
+class LaputaConfig
+{
+public:
+	LaputaConfig();
+	virtual ~LaputaConfig();
+	EEPROM_STRUCT context;
+	bool load_config();
+	void save_config();
+private:
+	bool calc_chksum();
+};
+
+#endif /* LAPUTACONFIG_H_ */
